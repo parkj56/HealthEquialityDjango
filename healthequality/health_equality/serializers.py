6e2cfb19e18916b82d59ebff9a5c463334cf78c0
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-11-11 16:11:21 -0600

    updated serializers

--- healthequality/health_equality/serializers.py
+++ healthequality/health_equality/serializers.py
@@ -1,7 +1,7 @@
-# from rest_framework import serializers
-# from .models import BodyData 
+from rest_framework import serializers
+from .models import BodyData 
 
-# class SongSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = BodyData
-#         fields = ['id', 'title', 'artist', 'album', 'genre', 'release_date']
+class BDSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BodyData
+        fields = ['age', 'height', 'weight', 'dose', 'start_date', 'med_date']
